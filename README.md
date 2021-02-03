9ccedc1c7cada4a6676bbd261d816a6edb7fde62
Rajmohan <37916448+rajmohanghantasala@users.noreply.github.com>
2021-02-03 10:45:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# What is this ?
+
+Get Perfect shadows every time for the non-designer.
+
+# Installation
+`npm i  shadowimage --save`
+
+Then...
+
+```
+import { shadowimage } from 'shadowimage';
+
+shadowimage({
+    shadow_type:'soft',
+    padding:false
+});
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+function shadowimage(options) {
+    let images = document.querySelectorAll('.shadowimage');
+
+    if (options.shadow_type == 'hard') {
+        options.shadow_type = '0px'
+    } else {
+        options.shadow_type = '15px'
+    }
+    images.forEach(images=>{
+        images.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+        if (options.padding) {
+            images.style.padding = '1em';
+        }
+    })
+}
+
+module.exports.shadowimage = shadowimage;
